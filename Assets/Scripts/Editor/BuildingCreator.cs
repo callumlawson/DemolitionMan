7244051558f8de3c069fef59942a0d267ed8b7d7
Callum Lawson <lawsonathome@gmail.com>
2018-12-19 11:32:18 +0000

    Up to date

--- Assets/Scripts/Editor/BuildingCreator.cs
+++ Assets/Scripts/Editor/BuildingCreator.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class BuildingCreator : EditorWindow
 {
     private GameObject root;
-    private List<BeamData>[,] beamDatas;
+    private List<BeamData>[,,] beamDatas;
     private int width = 6;
     private int height = 6;
+    private int depth = 6;
+    private int beamLength = 3;
+    private int beamMass = 10;
+    private float breakForce = 10000;
+    private float breakTorque = 10000;
+    private float spawnHeightOffset = 0.1f;
+    private float linearDrag = 0.1f;
+    private float angularDrag = 0.1f;
 
-    public class BeamData
+    private class BeamData
     {
         public GameObject beam;
         public Vector3 rootPosition;
         public Vector3 rootDirection;
-        public (int, int)[] connections;
+        public (int, int, int)[] connections;
     }
 
     [MenuItem("DemolitionMan/Building Creator")]
@@ -27,26 +35,35 @@ public class BuildingCreator : EditorWindow
     {
         width = EditorGUILayout.IntField("Width", width);
         height = EditorGUILayout.IntField("Height", height);
+        depth = EditorGUILayout.IntField("Depth", depth);
+        beamLength = EditorGUILayout.IntField("Beam Length", beamLength);
+        beamMass = EditorGUILayout.IntField("Beam Mass", beamMass);
+        breakForce = EditorGUILayout.FloatField("Break Force", breakForce);
+        breakTorque = EditorGUILayout.FloatField("Break Torque", breakTorque);
+        linearDrag = EditorGUILayout.FloatField("Linear Drag", linearDrag);
+        angularDrag = EditorGUILayout.FloatField("Angular Drag", angularDrag);
         if (GUILayout.Button("Generate"))
         {
-            GenerateBuilding(width, height);
+            GenerateBuilding(width, height, depth);
         }
     }
 
-    private void GenerateBuilding(int width, int height)
+    private void GenerateBuilding(int width, int height, int depth)
     {
         Debug.Log("Generating!");
         //Setup root
-        beamDatas = new List<BeamData>[width,height];
+        beamDatas = new List<BeamData>[width,height,depth];
         DestroyImmediate(root);
         root = new GameObject($"BuildingRoot: Width {width}, Height {height}");
 
-        CreateBeams(width, height);
+        //Create beamDatas
+        CreateBeams(width, height, depth);
+
         PrintBeams(beamDatas);
         JoinBeams(beamDatas);
     }
 
-    private void PrintBeams(List<BeamData>[,] beamDatas)
+    private static void PrintBeams(List<BeamData>[,,] beamDatas)
     {
         foreach (var beamList in beamDatas)
         {
@@ -57,92 +74,119 @@ public class BuildingCreator : EditorWindow
         }
     }
 
-    private void PrintBeam(BeamData data)
+    private static void PrintBeam(BeamData data)
     {
         Debug.Log($"Position: {data.rootPosition} Direction: {data.rootDirection} Connections: {data.connections}");
     }
 
-    private void CreateBeams(int width, int height)
+    private void CreateBeams(int width, int height, int depth)
     {
         for (var w = 0; w < width; w++)
         {
             for (var h = 0; h < height; h++)
             {
-                beamDatas[w, h] = new List<BeamData>();
+                for (var d = 0; d < depth; d++)
+                {
+                    beamDatas[w, h, d] = new List<BeamData>();
 
-                //Verticals
-                var vertBeamRootPosition = new Vector3(w, h, 0);
-                var vertBeamRootRotation = Vector3.up;
-                var vertBeam = SpawnBeam(vertBeamRootPosition, vertBeamRootRotation);
-                var vertBeamData = new BeamData
-                {
-                    rootPosition = vertBeamRootPosition,
-                    rootDirection = vertBeamRootRotation,
-                    beam = vertBeam,
-                    connections = h + 1 < height ? new (int, int)[]{(w, h + 1), (w, h)} : new (int, int)[] { (w, h) }
-                };
-                beamDatas[w, h].Add(vertBeamData);
-                
-                //Horizontals
-                if (w < width - 1)
-                {
-                    var hozBeamRootPosition = new Vector3(w, h, 0);
-                    var hozBeamRootRotation = Vector3.right;
-                    var hozBeam = SpawnBeam(hozBeamRootPosition, hozBeamRootRotation);
-                    var hozBeamData = new BeamData
+                    if (h < height - 1)
                     {
-                        rootPosition = hozBeamRootPosition,
-                        rootDirection = hozBeamRootRotation,
-                        beam = hozBeam,
-                        connections = new(int, int)[] { (w, h), (w + 1, h) }
-                    };
-                    beamDatas[w, h].Add(hozBeamData);
-                } 
+                        //Verticals
+                        var vertBeamRootPosition = new Vector3(w * beamLength, h * beamLength + spawnHeightOffset,
+                            d * beamLength);
+                        var vertBeamRootRotation = Vector3.up;
+                        var vertBeam = SpawnBeam(vertBeamRootPosition, vertBeamRootRotation);
+                        var vertBeamData = new BeamData
+                        {
+                            rootPosition = vertBeamRootPosition,
+                            rootDirection = vertBeamRootRotation,
+                            beam = vertBeam,
+                            connections = new(int, int, int)[] { (w, h, d), (w, h + 1, d) }
+                        };
+                        beamDatas[w, h, d].Add(vertBeamData);
+                    }
+
+                    //Horizontals x
+                    if (w < width - 1)
+                    {
+                        var hozBeamRootPosition = new Vector3(w * beamLength, h * beamLength + spawnHeightOffset, d * beamLength);
+                        var hozBeamRootRotation = Vector3.right;
+                        var hozBeam = SpawnBeam(hozBeamRootPosition, hozBeamRootRotation);
+                        var hozBeamData = new BeamData
+                        {
+                            rootPosition = hozBeamRootPosition,
+                            rootDirection = hozBeamRootRotation,
+                            beam = hozBeam,
+                            connections = new (int, int, int)[] {(w, h, d), (w + 1, h, d)}
+                        };
+                        beamDatas[w, h, d].Add(hozBeamData);
+                    }
+
+                    //Horizontals z
+                    if (d < depth - 1)
+                    {
+                        var hozBeamRootPosition = new Vector3(w * beamLength, h * beamLength + spawnHeightOffset, d * beamLength);
+                        var hozBeamRootRotation = Vector3.forward;
+                        var hozBeam = SpawnBeam(hozBeamRootPosition, hozBeamRootRotation);
+                        var hozBeamData = new BeamData
+                        {
+                            rootPosition = hozBeamRootPosition,
+                            rootDirection = hozBeamRootRotation,
+                            beam = hozBeam,
+                            connections = new(int, int, int)[] { (w, h, d), (w, h, d + 1) }
+                        };
+                        beamDatas[w, h, d].Add(hozBeamData);
+                    }
+                }
             }
         }
     }
 
-    private void JoinBeams(List<BeamData>[,] beamDatas)
+    private void JoinBeams(List<BeamData>[,,] beamDatas)
     {
         for (var w = 0; w < beamDatas.GetLength(0); w++)
         {
             for (var h = 0; h < beamDatas.GetLength(1); h++)
             {
-                //This is awful and I'm sorry
-                foreach (var beamData in beamDatas[w, h])
+                for (var d = 0; d < beamDatas.GetLength(2); d++)
                 {
-                    var thisBeam = beamData.beam;
-                    foreach (var connection in beamData.connections)
+                    //This is awful and I'm sorry
+                    foreach (var beamData in beamDatas[w, h, d])
                     {
-                        var beamsToConnectTo = beamDatas[connection.Item1, connection.Item2];
-                        foreach (var beamToConnect in beamsToConnectTo)
+                        var thisBeam = beamData.beam;
+                        foreach (var connection in beamData.connections)
                         {
-                            if (beamToConnect.beam != thisBeam)
+                            var beamsToConnectTo = beamDatas[connection.Item1, connection.Item2, connection.Item3];
+                            foreach (var beamToConnect in beamsToConnectTo)
                             {
-                                var joint = thisBeam.AddComponent<ConfigurableJoint>();
-                                joint.xMotion = ConfigurableJointMotion.Locked;
-                                joint.yMotion = ConfigurableJointMotion.Locked;
-                                joint.zMotion = ConfigurableJointMotion.Locked;
-                                joint.angularXMotion = ConfigurableJointMotion.Locked;
-                                joint.angularYMotion = ConfigurableJointMotion.Locked;
-                                joint.angularZMotion = ConfigurableJointMotion.Locked;
-                                joint.projectionMode = JointProjectionMode.None;
-                                joint.projectionDistance = 0.3f;
-                                joint.projectionAngle = 5f;
-                                joint.breakForce = 5000;
-                                joint.breakTorque = 3000;
-                                joint.connectedBody = beamToConnect.beam.GetComponent<Rigidbody>();
-                                joint.enablePreprocessing = false;
-                                //If we are connecting to things at our root
-                                if (connection.Item1 == w && connection.Item2 == h)
+                                if (beamToConnect.beam != thisBeam)
                                 {
-                                    joint.anchor = Vector3.zero;
+                                    var joint = thisBeam.AddComponent<ConfigurableJoint>();
+                                    joint.xMotion = ConfigurableJointMotion.Locked;
+                                    joint.yMotion = ConfigurableJointMotion.Locked;
+                                    joint.zMotion = ConfigurableJointMotion.Locked;
+                                    joint.angularXMotion = ConfigurableJointMotion.Locked;
+                                    joint.angularYMotion = ConfigurableJointMotion.Locked;
+                                    joint.angularZMotion = ConfigurableJointMotion.Locked;
+                                    joint.projectionMode = JointProjectionMode.None;
+                                    joint.projectionDistance = 5f;
+                                    joint.projectionAngle = 20f;
+                                    joint.breakForce = 10000;
+                                    joint.breakTorque = 7000;
+                                    joint.connectedBody = beamToConnect.beam.GetComponent<Rigidbody>();
+                                    joint.enablePreprocessing = false;
+                                    joint.enableCollision = false;
+                                    //If we are connecting to things at our root
+                                    if (connection.Item1 == w && connection.Item2 == h)
+                                    {
+                                        joint.anchor = Vector3.zero;
+                                    }
+                                    else
+                                    {
+                                        joint.anchor = Vector3.forward * beamLength;
+                                    }
+                                    joint.autoConfigureConnectedAnchor = true;
                                 }
-                                else
-                                {
-                                    joint.anchor = Vector3.forward;
-                                }
-                                joint.autoConfigureConnectedAnchor = true;
                             }
                         }
                     }
@@ -157,7 +201,11 @@ public class BuildingCreator : EditorWindow
         beam.transform.position = new Vector3(rootPosition.x, rootPosition.y, rootPosition.z);
         beam.transform.rotation = Quaternion.LookRotation(rootRotation);
         beam.transform.parent = root.transform;
-        beam.GetComponent<Rigidbody>().mass = 10;
+        var rb = beam.GetComponent<Rigidbody>();
+        rb.mass = beamMass;
+        rb.drag = linearDrag;
+        rb.angularDrag = angularDrag;
+        rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
         return beam;
     }
 }
---
+++ Assets/Scripts/HighExplosive.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class HighExplosive : MonoBehaviour
+{
+    public int Radius;
+    public int ExplosionForce;
+    public float DelaySeconds;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Invoke(nameof(Explode), DelaySeconds);
+        }
+    }
+
+    void Explode()
+    {
+        var explosivePosition = gameObject.transform.position;
+        //Not going to work well with multiple colliders per object!
+        var affectedObjects = Physics.OverlapSphere(transform.position, Radius);
+        foreach (var coll in affectedObjects)
+        {
+            var rb = coll.attachedRigidbody;
+            if (rb != null)
+            {
+                var center = coll.bounds.center;
+                var vectorToTarget = rb.position - explosivePosition;
+                var distance = Vector3.Magnitude(vectorToTarget);
+                var directionVector = Vector3.Normalize(vectorToTarget);
+                rb.AddForceAtPosition(directionVector * ExplosionForce * 1 / distance, center);
+            }
+        }
+    }
+}
