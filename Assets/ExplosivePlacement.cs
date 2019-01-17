957d3e49f9cc8261b8810eeddb29ce2e9b60d32c
Callum Lawson <lawsonathome@gmail.com>
2019-01-17 17:32:18 +0000

    Added Character Controller and Skybox

---
+++ Assets/ExplosivePlacement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController.Examples;
+using UnityEngine;
+
+public class ExplosivePlacement : MonoBehaviour
+{
+    public ExampleCharacterCamera OurCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
